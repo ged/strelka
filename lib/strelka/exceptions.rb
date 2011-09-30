f95208dea19f8599179f0f85bfff1242133bea25
Michael Granger <ged@FaerieMUD.org>
2011-09-30 16:17:28 -0700

    Adding an exceptions module, add Strelka::Error class

---
+++ lib/strelka/exceptions.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+#encoding: utf-8
+
+# Exception types used by Strelka classes.
+
+#--
+module Strelka
+
+	# A base exception class.
+	class Error < ::RuntimeError; end
+
+
+end # module Strelka
+
