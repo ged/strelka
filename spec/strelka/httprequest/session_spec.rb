9eccd34084c35013532b04d1157af558796a9e13
Michael Granger <ged@FaerieMUD.org>
2012-03-27 18:32:16 -0700

    Add specs for Strelka::HTTPRequest::Session.

--- spec/strelka/httprequest/session_spec.rb
+++ spec/strelka/httprequest/session_spec.rb
@@ -16,7 +16,9 @@ require 'rspec'
 require 'spec/lib/helpers'
 
 require 'strelka'
+require 'strelka/app/sessions'
 require 'strelka/httprequest/session'
+require 'strelka/session/default'
 
 
 #####################################################################
@@ -28,16 +30,74 @@ describe Strelka::HTTPRequest::Session do
 	before( :all ) do
 		setup_logging( :fatal )
 		@request_factory = Mongrel2::RequestFactory.new( route: '/service/user' )
+		Strelka::App::Sessions.configure( session_class: 'default' )
+	end
+
+	before( :each ) do
+		@req = @request_factory.get( '/service/user/estark' )
+		Strelka.log.debug "Extending request %p" % [ @req ]
+		@req.extend( described_class )
+	end
+
+	after( :each ) do
+		Strelka::Session::Default.sessions.clear
 	end
 
 	after( :all ) do
 		reset_logging()
 	end
 
-	before( :each ) do
-		@req = @request_factory.get( '/service/user/estark' )
-		@req.extend( described_class )
+
+	describe "an HTTPRequest with no session loaded" do
+
+		it "has a session_namespace attribute" do
+			@req.should respond_to( :session_namespace )
+		end
+
+		it "knows whether or not it has loaded a session" do
+			@req.session?.should be_false()
+		end
+
+		it "doesn't load the session when the session namespace is set" do
+			@req.session_namespace = 'an_appid'
+			@req.session?.should be_false()
+		end
+
+		it "loads the session as soon as it's accessed" do
+			@req.session.should be_a( Strelka::Session::Default )
+		end
+
+		it "sets the session's namespace when it's loaded" do
+			@req.session_namespace = 'an_appid'
+			@req.session.namespace.should == :an_appid
+		end
+
+		it "sets a session's namespace when it's set directly" do
+			@req.should respond_to( :session_namespace= )
+			@req.session_namespace = 'the_appid'
+
+			session = mock( "session object" )
+			session.should_receive( :namespace= ).with( 'the_appid' )
+
+			@req.session = session
+		end
+
 	end
 
+
+	describe "an HTTPRequest with a session loaded" do
+
+		before( :each ) do
+			@req.session_namespace = 'other_appid'
+			@req.session
+		end
+
+		it "sets its session's namespace when its session_namespace attribute is set" do
+			@req.session.namespace.should == :other_appid
+			@req.session_namespace = 'an_appid'
+			@req.session.namespace.should == :an_appid
+		end
+
+	end
 
 end
