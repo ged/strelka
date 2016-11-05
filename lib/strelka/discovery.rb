4e758392f1fdc88212a644502efbd85307f41728
Michael Granger <ged@FaerieMUD.org>
2016-11-05 13:57:43 -0700

    Added docs for discovery

--- lib/strelka/discovery.rb
+++ lib/strelka/discovery.rb
@@ -7,6 +7,83 @@ require 'strelka' unless defined?( Strelka )
 
 
 # The Strelka application-discovery system.
+#
+# This module provides a mechanism for registering Strelka apps and their
+# resources for discovery by the strelka CLI and other systems.
+#
+# It's responsible for three kinds of discovery:
+#
+# - Discovery of Strelka app files via Rubygems discovery
+# - Discovery and loading of Strelka app classes by name
+# - Discovery of data directories for strelka apps
+#
+# As such it can be used in several different ways.
+#
+# == \App File \Discovery
+#
+# If you have an app that you wish to be discoverable, create a
+# <tt>lib/strelka/apps.rb</tt> file. This file will be added to those returned
+# by the ::app_discovery_files call, which is the list loaded by
+# ::discovered_apps. 
+#
+# == \App \Discovery Registration
+#
+# To add a name and file path to Strelka::Discovery.discovered_apps, you can
+# call ::register_app. This will check to make sure no other apps are registered
+# with the same name. To register several at the same time, call
+# ::register_apps with a Hash of <tt>name => path</tt> pairs.
+#
+# == Loading Discovered Apps
+#
+# To load a discovered app, call ::load with its registered name.
+#
+# This will load the associated file and returns the first Ruby class to inherit
+# from a discoverable app class like Strelka::App or Strelka::WebSocketServer.
+#
+# === Putting it all together
+#
+# Say, for example, you were putting together an <tt>acme-apps</tt> gem for the
+# Acme company that contained Strelka apps for a web store and a CMS. You could
+# add a <tt>lib/strelka/apps.rb</tt> file to the <tt>acme-apps</tt> gem that
+# contained the following:
+#
+#     # -*- ruby -*-
+#     require 'strelka/discovery'
+#
+#     Strelka::Discovery.register_apps(
+#         'acme-store' => 'lib/acme/store.rb',
+#         'acme-cms' => 'lib/acme/cms.rb'
+#     )
+#
+# This would let you do:
+#
+#     $ gem install acme-apps
+#     $ strelka start acme-store
+#
+#
+# == Data Directory \Discovery
+#
+# If your app requires some filesystem resources, a good way to distribute these
+# is in your gem's "data directory". This is a directory in your gem called
+# <tt>data/«your gem name»</tt>, and can be found via:
+#
+#    Gem.datadir( your_gem_name )
+#
+# Strelka::Discoverable builds on top of this, and can return a Hash of glob
+# patterns that will match the data directories of all gems that depend on
+# Strelka, keyed by gem name. You can use this to populate search paths for
+# templates, static assets, etc.
+#
+#    template_paths = Strelka::Discovery.discover_data_dirs.
+#        flat_map {|_, pattern| Dir.glob(pattern + '/templates') }
+#
+# == Making a class Discoverable
+#
+# If you write your own app base class (e.g., Strelka::App,
+# Strelka::WebSocketServer), you can make it discoverable by extending it with
+# this module. You typically won't have to do this unless you're working on
+# Strelka itself.
+#
 module Strelka::Discovery
 	extend Loggability,
 	       Configurability,
