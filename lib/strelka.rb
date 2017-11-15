a04a993ab9e3b34bdf2a427b4d75cd85853bf25b
Michael Granger <ged@FaerieMUD.org>
2017-11-15 15:48:13 -0800

    Backport flexible config-loading

--- lib/strelka.rb
+++ lib/strelka.rb
@@ -21,19 +21,32 @@ require 'configurability/config'
 module Strelka
 	extend Loggability
 
-	# Loggability API -- Set up this module as a log host.
-	log_as :strelka
-
 	# Library version constant
 	VERSION = '0.15.0'
 
 	# Version-control revision constant
 	REVISION = %q$Revision$
 
+	# The name of the environment variable that can be used to specify a configfile
+	# to load.
+	CONFIG_ENV = 'STRELKA_CONFIG'
+
+	# The name of the config file for local overrides.
+	LOCAL_CONFIG_FILE = Pathname( '~/.strelka.yml' ).expand_path
+
+	# The name of the config file that's loaded if none is specified.
+	DEFAULT_CONFIG_FILE = Pathname( 'config.yml' ).expand_path
+
+
+	# Loggability API -- set up a logger for this namespace.
+	log_as :strelka
+
+
 	require 'strelka/mixins'
 	require 'strelka/constants'
 	require 'strelka/exceptions'
 	include Strelka::Constants
+	extend Strelka::MethodUtilities
 
 	require 'strelka/app'
 	require 'strelka/httprequest'
@@ -50,19 +63,81 @@ module Strelka
 	end
 
 
-	# The installed Configurability::Config object
-	@config = nil
-	class << self; attr_accessor :config; end
+	### Get the library version. If +include_buildnum+ is true, the version string will
+	### include the VCS rev ID.
+	def self::version_string( include_buildnum=false )
+		vstring = "%s %s" % [ self.name, VERSION ]
+		vstring << " (build %s)" % [ REVISION[/: ([[:xdigit:]]+)/, 1] || '0' ] if include_buildnum
+		return vstring
+	end
 
 
-	### Convenience method -- Load the Configurability::Config from +configfile+
-	### and install it.
-	def self::load_config( configfile, defaults=nil )
-		defaults ||= Configurability.gather_defaults
-		self.log.info "Loading universal config from %p with defaults for sections: %p." %
-			[ configfile, defaults.keys ]
-		self.config = Configurability::Config.load( configfile, defaults )
-		self.config.install
+	##
+	# An Array of callbacks to be run after the config is loaded
+	singleton_attr_reader :after_configure_hooks
+	@after_configure_hooks = Set.new
+
+	##
+	# True if the after_configure hooks have already (started to) run.
+	singleton_predicate_reader :after_configure_hooks_run
+	@after_configure_hooks_run = false
+
+
+	#
+	# :section: Configuration API
+	#
+
+	### Get the loaded config (a Configurability::Config object)
+	def self::config
+		Configurability.loaded_config
+	end
+
+
+	### Returns +true+ if the configuration has been loaded at least once.
+	def self::config_loaded?
+		return self.config ? true : false
+	end
+
+
+	### Register a callback to be run after the config is loaded.
+	def self::after_configure( &block )
+		raise LocalJumpError, "no block given" unless block
+		self.after_configure_hooks << block
+
+		# Call the block immediately if the hooks have already been called or are in
+		# the process of being called.
+		block.call if self.after_configure_hooks_run?
+	end
+	singleton_method_alias :after_configuration, :after_configure
+
+
+	### Call the post-configuration callbacks.
+	def self::call_after_configure_hooks
+		self.log.debug "  calling %d post-config hooks" % [ self.after_configure_hooks.length ]
+		@after_configure_hooks_run = true
+
+		self.after_configure_hooks.to_a.each do |hook|
+			self.log.debug "    %s line %s..." % hook.source_location
+			hook.call
+		end
+	end
+
+
+	### Load the specified +config_file+, install the config in all objects with
+	### Configurability, and call any callbacks registered via #after_configure.
+	def self::load_config( config_file=nil, defaults=nil )
+		config_file ||= ENV[ CONFIG_ENV ]
+		config_file ||= LOCAL_CONFIG_FILE if LOCAL_CONFIG_FILE.exist?
+		config_file ||= DEFAULT_CONFIG_FILE
+
+		defaults    ||= Configurability.gather_defaults
+
+		self.log.warn "Loading config from %p with defaults for sections: %p." %
+			[ config_file, defaults.keys ]
+		config = Configurability::Config.load( config_file, defaults )
+		config.install
+
+		self.call_after_configure_hooks
 	end
 
 
--- spec/strelka_spec.rb
+++ spec/strelka_spec.rb
@@ -7,7 +7,23 @@ require 'strelka'
 
 describe Strelka do
 
+	before( :all ) do
+		@original_config_env = ENV[Strelka::CONFIG_ENV]
+	end
+
+	before( :each ) do
+		ENV.delete(Strelka::CONFIG_ENV)
+		Strelka.after_configure_hooks.clear
+	end
+
+	after( :all ) do
+		ENV[Strelka::CONFIG_ENV] = @original_config_env
+	end
+
+
+
 	describe "version methods" do
+
 		it "returns a version string if asked" do
 			expect( described_class.version_string ).to match( /\w+ [\d.]+/ )
 		end
@@ -17,7 +33,159 @@ describe Strelka do
 			expect( described_class.version_string(true) ).
 				to match(/\w+ [\d.]+ \(build [[:xdigit:]]+\)/)
 		end
+
 	end
+
+
+	# let( :config ) { Configurability::Config.new(TESTING_CONFIG_SOURCE) }
+
+
+	it "will load a local config file if it exists and none is specified" do
+		config_object = double( "Configurability::Config object" )
+		allow( config_object ).to receive( :[] ).with( :strelka ).and_return( {} )
+
+		expect( Configurability ).to receive( :gather_defaults ).
+			and_return( {} )
+		expect( Strelka::LOCAL_CONFIG_FILE ).to receive( :exist? ).
+			and_return( true )
+		expect( Configurability::Config ).to receive( :load ).
+			with( Strelka::LOCAL_CONFIG_FILE, {} ).
+			and_return( config_object )
+		expect( config_object ).to receive( :install )
+
+		Strelka.load_config
+	end
+
+
+	it "will load a default config file if none is specified and there's no local config" do
+		config_object = double( "Configurability::Config object" )
+		allow( config_object ).to receive( :[] ).with( :strelka ).and_return( {} )
+
+		expect( Configurability ).to receive( :gather_defaults ).
+			and_return( {} )
+		expect( Strelka::LOCAL_CONFIG_FILE ).to receive( :exist? ).
+			and_return( false )
+		expect( Configurability::Config ).to receive( :load ).
+			with( Strelka::DEFAULT_CONFIG_FILE, {} ).
+			and_return( config_object )
+		expect( config_object ).to receive( :install )
+
+		Strelka.load_config
+	end
+
+
+	it "will load a config file given in an environment variable" do
+		ENV['STRELKA_CONFIG'] = '/usr/local/etc/strelka.yml'
+
+		config_object = double( "Configurability::Config object" )
+		allow( config_object ).to receive( :[] ).with( :strelka ).and_return( {} )
+
+		expect( Configurability ).to receive( :gather_defaults ).
+			and_return( {} )
+		expect( Configurability::Config ).to receive( :load ).
+			with( '/usr/local/etc/strelka.yml', {} ).
+			and_return( config_object )
+		expect( config_object ).to receive( :install )
+
+		Strelka.load_config
+	end
+
+
+	it "will load a config file and install it if one is given" do
+		config_object = double( "Configurability::Config object" )
+		allow( config_object ).to receive( :[] ).with( :strelka ).and_return( {} )
+
+		expect( Configurability ).to receive( :gather_defaults ).
+			and_return( {} )
+		expect( Configurability::Config ).to receive( :load ).
+			with( 'a/configfile.yml', {} ).
+			and_return( config_object )
+		expect( config_object ).to receive( :install )
+
+		Strelka.load_config( 'a/configfile.yml' )
+	end
+
+
+	it "will override default values when loading the config if they're given" do
+		config_object = double( "Configurability::Config object" )
+		allow( config_object ).to receive( :[] ).with( :strelka ).and_return( {} )
+
+		expect( Configurability ).to_not receive( :gather_defaults )
+		expect( Configurability::Config ).to receive( :load ).
+			with( 'a/different/configfile.yml', {database: {dbname: 'test'}} ).
+			and_return( config_object )
+		expect( config_object ).to receive( :install )
+
+		Strelka.load_config( 'a/different/configfile.yml', database: {dbname: 'test'} )
+	end
+
+
+	it "will call any registered callbacks after the config is installed" do
+		config_object = double( "Configurability::Config object" )
+		allow( config_object ).to receive( :[] ).with( :strelka ).and_return( {} )
+
+		expect( Configurability ).to receive( :gather_defaults ).
+			and_return( {} )
+		expect( Configurability::Config ).to receive( :load ).
+			with( 'a/configfile.yml', {} ).
+			and_return( config_object )
+		expect( config_object ).to receive( :install )
+
+		hook_was_called = false
+		Strelka.after_configure do
+			hook_was_called = true
+		end
+		Strelka.load_config( 'a/configfile.yml' )
+
+		expect( hook_was_called ).to be( true )
+	end
+
+
+	it "will immediately call after_config callbacks registered after the config is installed" do
+		config_object = double( "Configurability::Config object" )
+		allow( config_object ).to receive( :[] ).with( :strelka ).and_return( {} )
+
+		expect( Configurability ).to receive( :gather_defaults ).
+			and_return( {} )
+		expect( Configurability::Config ).to receive( :load ).
+			with( 'a/configfile.yml', {} ).
+			and_return( config_object )
+		expect( config_object ).to receive( :install )
+
+		hook_was_called = false
+		Strelka.load_config( 'a/configfile.yml' )
+
+		Strelka.after_configure do
+			hook_was_called = true
+		end
+
+		expect( hook_was_called ).to be( true )
+	end
+
+
+	it "can add new after_configure hooks even while the current ones are being run" do
+		config_object = double( "Configurability::Config object" )
+		allow( config_object ).to receive( :[] ).with( :strelka ).and_return( {} )
+
+		expect( Configurability ).to receive( :gather_defaults ).
+			and_return( {} )
+		expect( Configurability::Config ).to receive( :load ).
+			with( 'a/configfile.yml', {} ).
+			and_return( config_object )
+		expect( config_object ).to receive( :install )
+
+		hook_was_called = false
+		Strelka.after_configure do
+			Strelka.after_configure do
+				hook_was_called = true
+			end
+		end
+
+		Strelka.load_config( 'a/configfile.yml' )
+
+		expect( hook_was_called ).to be( true )
+	end
+
 
 end
 
