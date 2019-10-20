ed01467e71ceec35a2a8ffec694deb93b255f396
Michael Granger <ged@FaerieMUD.org>
2019-10-20 15:46:12 -0700

    Unhoeify and update
    - Remove hoe and use rake tasks
    - Remove some old documentation stuff
    - Update license dates
    - Remove Bitbucket and set URLs to Sourcehut

--- gem.deps.rb
+++ gem.deps.rb
@@ -1,2 +1,21 @@
 source "https://rubygems.org/"
-gemspec
+
+gem 'configurability', '~> 3.1'
+gem 'foreman', '~> 0.62'
+gem 'highline', '~> 1.6'
+gem 'inversion', '~> 1.0'
+gem 'loggability', '~> 0.9'
+gem 'mongrel2', '~> 0.53'
+gem 'pluggability', '~> 0.4'
+gem 'sysexits', '~> 1.1'
+gem 'uuidtools', '~> 2.1'
+gem 'safe_yaml', '~> 1.0'
+gem 'gli', '~> 2.14'
+
+group( :development ) do
+	gem 'rake-deveiate', '~> 0.4'
+	gem 'rspec', '~> 3.8'
+	gem 'simplecov', '~> 0.7'
+	gem 'rdoc-generator-fivefish', '~> 0.1'
+end
+
